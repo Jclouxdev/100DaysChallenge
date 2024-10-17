80d362b68a2981877a341f208dfd2be703aac247
Jclouxdev <joseph.clx@gmail.com>
2024-10-17 21:50:45 +0200

    :rocket: 009 Password Generator

--- src/routes/09_PasswordGenerator/utils/passwordGeneration.tsx
+++ src/routes/09_PasswordGenerator/utils/passwordGeneration.tsx
@@ -28,17 +28,43 @@ enum Alphabet {
 }
   
 
-export const passwordGeneration = (size:number):string => {
-    let str = ""
+export const passwordGeneration = (size:number, upper:boolean, lower:boolean, numb:boolean, symbol:boolean):string => {
+    let str:string[] = []
     for(let i = 0; i < size; i++) {
-        str+=randomOneLetter()
+        if(upper) str.push(randomOneLetterUppercase())
+        if(lower) str.push(randomOneLetterLowercase())
+        if(numb) str.push(randomOneToNine().toString())
+        if(symbol) str.push(getRandomSymbol())
     }
-    return str
+    return shuffleArray(str).slice(0, size).join("")
 }
 
-const randomOneLetter = ():Alphabet => {
+const randomOneLetterUppercase = ():Alphabet => {
     const randomIndex = Math.floor(Math.random() * 24) + 1
-
     const letters = Object.values(Alphabet)
     return letters[randomIndex - 1];
-} 
+}
+
+const randomOneLetterLowercase = ():string => {
+    const randomIndex = Math.floor(Math.random() * 24) + 1
+    const letters = Object.values(Alphabet)
+    return letters[randomIndex - 1].toLocaleLowerCase();
+}
+
+const randomOneToNine = ():number => {
+    return Math.floor(Math.random() * 9) + 1;
+}
+
+const getRandomSymbol = (): string => {
+    const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+    const randomIndex = Math.floor(Math.random() * symbols.length);
+    return symbols[randomIndex];
+}
+
+const shuffleArray = (array: any[]): any[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
