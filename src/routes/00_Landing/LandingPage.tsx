1a4e49308cade138e957f12503e357ea6c865ccc
Jclouxdev <joseph.clx@gmail.com>
2024-10-18 11:53:11 +0200

    :lipstick: Fix LP

--- src/routes/00_Landing/LandingPage.tsx
+++ src/routes/00_Landing/LandingPage.tsx
@@ -21,14 +21,14 @@ export const LandingPage = () => {
                 <button onClick={handleScrollToAbout} className="bg-[#2F8ED0] text-white py-3 px-6 rounded font-bold z-10">
                     En apprendre plus sur ce défi
                 </button>
-                <img src={WaveBg} alt="" className='absolute bottom-20 md:bottom-0 left-0 z-0' />
+                <img src={WaveBg} alt="" className='absolute bottom-0 left-0 z-0' />
             </section>
-            <section className='pb-12 px-6 md:px-32 text-[#F7F7F7] -translate-y-20'
+            <section className='pb-12 px-6 md:px-32 text-[#F7F7F7]'
                 style={{
                     background:'rgb(0,29,112) linear-gradient(180deg, rgba(0,29,112,1) 0%, rgba(0,12,46,1) 100%)',
                 }}
             >
-                <h2 id="about" className="text-xl font-bold font-montserat mb-4 pt-40">Pourquoi ai-je décidé de relever ce défi de 100 jours ?</h2>
+                <h2 id="about" className="text-xl font-bold font-montserat mb-4 pt-20">Pourquoi ai-je décidé de relever ce défi de 100 jours ?</h2>
                 <p className='font-light'>J'ai choisi de consacrer 100 jours à ce défi pour me focaliser sur le développement front-end et acquérir des habitudes et réflexes solides. En codant quotidiennement, je compte repousser mes limites, créer des composants modernes et cohérents, et construire un portfolio qui reflète mes compétences actuelles</p>
                 <div className='grid gap-4 mt-4'>
                     <span className='flex gap-2 items-center text-[#2F8ED0]'>
@@ -97,7 +97,7 @@ export const LandingPage = () => {
                     </div>
                 </div>
             </section>
-            <footer className='bg-[#000616] text-[#616779] flex w-full justify-evenly p-4 -translate-y-20'>
+            <footer className='bg-[#000616] text-[#616779] flex w-full justify-evenly p-4 '>
                 <p>© Jclouxdev</p>
                 <a href="https://github.com/Jclouxdev" className='flex gap-2'>
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
